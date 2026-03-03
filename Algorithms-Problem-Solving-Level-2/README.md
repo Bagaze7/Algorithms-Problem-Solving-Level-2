ad5870a85c6d79a2c70cd5310dfc809fc34e370b
bagaze7 <yassinebagaze7@gmail.com>
2026-03-03 17:31:45 +0000

    add: problem 15 - letter pattern

---
+++ Algorithms-Problem-Solving-Level-2/README.md
@@ -0,0 +1,9 @@
+﻿# Algorithms & Problem-Solving – Level 2
+
+50 مسألة برمجية بلغة C++
+
+## المسائل
+
+| # | المسألة |
+|---|---------|
+| 01 | [Hello World](problems/01_hello_world.cpp) |
---
+++ Problem_15_Letter_Pattern/15_letter_pattern.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;  // Variable to store the user's input.
+    do
+    {
+        cout << Message << endl;  // Display the prompt message.
+        cin >> Number;            // Read the number from the user.
+    } while (Number <= 0);        // Continue prompting if the number is not positive.
+
+    return Number;  // Return the validated positive number.
+}
+
+
+void PrintLetters(int Num) {
+    int plus = Num + 64;
+    for (int i = 65; i <= plus; i++) {
+        for (int j = 65; j <= i; j++) {
+            cout << (char)i;
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+
+    PrintLetters(ReadPositiveNumber("Please enter a positive number?"));
+
+    return 0;
+}
