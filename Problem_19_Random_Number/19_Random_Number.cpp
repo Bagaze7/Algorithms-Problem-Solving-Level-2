e97d11efdaa003acd3fa4653da068cbf186b6443
bagaze7 <yassinebagaze7@gmail.com>
2026-03-05 06:42:34 +0000

    add: problem 19 - random number generator

--- Problem_19_Random_Number/19_Random_Number.cpp
+++ Problem_19_Random_Number/19_Random_Number.cpp
@@ -21,73 +21,14 @@ int RandomNumber(int From, int To)
     int randNum = rand() % (To - From + 1) + From;
     return randNum;
 }
-void CreateArray(int arr[100], int& arrLength) {
-    do {
- cout << "\nEnter number of elements:\n";
-    cin >> arrLength;
-    } while (arrLength > 100);
-   
-    for (int i = 0; i < arrLength; i++) {
-        arr[i] = RandomNumber(1, 100); // Fill the array with random numbers between 1 and 100.
-    }
-}
-void PrintArrayy(int arr[100], int arrLength) {
-	cout << "Array 1 elements:" << endl;
-    for (int i = 0; i < arrLength; i++) {
-        cout << arr[i] << " "; // Print each element of the array followed by a space.
-    }
-    cout << "\n"; // Print a newline after printing all elements.
-}
-
-
-void CopyArray(int source[100], int destination[100], int arrLength) {
-	for (int i = 0; i < arrLength; i++) {
-		destination[i] = source[i]; // Copy each element from the source array to the destination array.
-	}
-}
-bool isPrime(int number) {
-	if (number <= 1) return false; // 0 and 1 are not prime numbers.
-	for (int i = 2; i <= number / 2; i++) { // Check divisibility from 2 to number/2.
-		if (number % i == 0) {
-			return false; // If the number is divisible by any of these, it's not prime.
-		}
-	}
-	return true; // If no divisors found, it's a prime number.
-}
-int GetPrimeNumbers(int arr[100], int arrLength, int SecondArr[100]) {
-    int count = 0;
-	for (int i = 0; i < arrLength; i++) {
-		if (isPrime(arr[i])) {
-			SecondArr[count] = arr[i]; // If the current element is prime, add it to the second array.
-			count++; // Increment the count of prime numbers found.
-		}
-	}
-    return count;
-}
-void PrintSecondArray(int SecondArr[100], int arrLength) {
-	if (arrLength == 0) {
-		cout << "No prime numbers found in the array." << endl; // If no prime numbers were found, print a message.
-		return;
-    }
-    else {
-    cout << "Prime numbers Array 2 : " << endl;
-    for (int i = 0; i < arrLength; i++) {
-        cout << SecondArr[i] << " "; // Print each prime number in the second array followed by a space.
-    }
-}
-    }
 
 
 int main()
 {
     srand((unsigned)time(NULL));
 
-	int SecondArr[100];
-    int arr[100];
-    int arrLength;
-	int secondArrLength;
-    CreateArray(arr, arrLength);
-    PrintArrayy(arr, arrLength);
-	PrintSecondArray(SecondArr, GetPrimeNumbers(arr, arrLength, SecondArr));
+	cout << RandomNumber(1, 100) << endl; // Generate and print a random number between 1 and 100.
+	cout << RandomNumber(1, 100) << endl; // Generate and print another random number between 1 and 100.
+	cout << RandomNumber(1, 100) << endl; // Generate and print yet another random number between 1 and 100.
     return 0;
 }
