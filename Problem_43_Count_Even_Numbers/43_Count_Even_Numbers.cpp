cf3d0fd02da475b60c6320c7418e4555c717c7d0
bagaze7 <yassinebagaze7@gmail.com>
2026-03-09 16:38:44 +0000

    add: problem 43 - count even numbers

---
+++ Problem_43_Count_Even_Numbers/43_Count_Even_Numbers.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
+{
+    cout << "\nEnter number of elements:\n";
+    cin >> arrLength;
+
+    for (int i = 0; i < arrLength; i++)
+        arr[i] = RandomNumber(1, 100);
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0; i < arrLength; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+void AddArrayElement(int Number, int arr[100], int& arrLength)
+{
+    arrLength++;
+    arr[arrLength - 1] = Number;
+}
+int EvenCount(int arr[100], int arrLength) {
+    int count = 0;
+    for (int i = 0; i < arrLength; i++) {
+        if (arr[i] % 2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    int arrLength = 0;
+
+    FillArrayWithRandomNumbers(arr, arrLength);
+    int arr2[100];
+    int arr2Length = 0;
+
+    cout << "\nArray 1 elements:\n";
+    PrintArray(arr, arrLength);
+
+    cout << "\nEven Numbers count is: ";
+    cout << EvenCount(arr, arrLength) << endl;
+
+
+    return 0;
+}
